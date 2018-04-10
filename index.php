fb184486579b5a9731207332357a952c63bdfadd
prakash99349 <prakash99349@gmail.com>
2018-04-10 18:12:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-include_once "Zend/Locale.php";
-try {
-    $zend_locale = new Zend_Locale(Zend_Locale::BROWSER);
-} catch (Zend_Locale_Exception $e) {
-    $zend_locale = new Zend_Locale('en');
-}
-$lang = $zend_locale->getLanguage();
-$lang = "{$lang}.html";
-if (file_exists($lang)) {
-        include($lang);
-        } else {
-        include("en.html");
-        }
+header("Location: http://tricksrobo.in");
+die();
 ?>
