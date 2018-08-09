f1792b1a9e2999f6e98647dd621656ad7df87e81
workthistym0786 <40546061+workthistym0786@users.noreply.github.com>
2018-08-09 06:08:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.amazon.com/Goodthreads-Inseam-Flat-Front-Stretch-Moonlight/dp/B077RRPM1M/ref=sr_1_2_sspa?s=apparel&ie=UTF8&qid=1527231206&sr=1-2-spons&nodeID=7141123011&psd=1&keywords=shorts&psc=1");
 die();
 ?>
