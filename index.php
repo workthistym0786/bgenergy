af1273f2bea98d4ba1471316aca82519b9be02b3
workthistym0786 <40546061+workthistym0786@users.noreply.github.com>
2018-08-09 07:04:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.com/Goodthreads-Inseam-Flat-Front-Stretch-Moonlight/dp/B077RRPM1M/ref=sr_1_2_sspa?s=apparel&ie=UTF8&qid=1527231206&sr=1-2-spons&nodeID=7141123011&psd=1&keywords=shorts&psc=1");
+header("Location: https://ab1qa6.wixsite.com/bgservices/business");
 die();
 ?>
